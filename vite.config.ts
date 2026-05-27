0a949d732bc0e41485474e1402bb4986211b61c0
Emilio-Aldean <emilio.aldean@gmail.com>
2026-05-27 17:48:22 -0500

    Avance modificacion .json

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Desarrollo-de-Aplicaciones-Web---Tutorial-React/",
   plugins: [react()],
 })
